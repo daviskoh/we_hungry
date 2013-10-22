54e64eb5cd4c74b023c435a6e5299f8ffa787daf
daviskoh <koh.davis.0@gmail.com>
2013-10-22 14:45:52 -0400

    create user function w/ form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131022181041) do
+ActiveRecord::Schema.define(version: 20131022184052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20131022181041) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "name",                            null: false
     t.boolean  "vege",            default: false
     t.boolean  "vegan",           default: false
     t.boolean  "lactose",         default: false
