31f236ea225017d88114cc0b07ec30a9c3fd85e9
daviskoh <koh.davis.0@gmail.com>
2013-10-25 12:48:18 -0400

    working signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   end
 
   def create
-      @user = User.new(user_params)
+    @user = User.new(user_params)
+    if @user.save
 
       redirect_to user_path(@user)
     # return to signup page
