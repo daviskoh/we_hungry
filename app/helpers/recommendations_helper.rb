8ee86bccf501f9e5875ac708f6ba8b0888d631fe
daviskoh <koh.davis.0@gmail.com>
2014-01-13 20:00:53 -0500

    adjust CI to allow for negative score aka push high downvoted ings farther down rank

--- app/helpers/recommendations_helper.rb
+++ app/helpers/recommendations_helper.rb
@@ -135,12 +135,13 @@ module RecommendationsHelper
   # use to rank ingredients #######################
 
   def ci_lower_bound(pos, n, confidence)
-    if n == 0
-      return 0
+    if pos == 0
+      return -(total - pos)
     end
+
     z = Statistics2.pnormaldist(1-(1-confidence)/2)
-    phat = 1.0*pos/n
-    (phat + z*z/(2*n) - z * Math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)
+    phat = (1.0 * pos) / total
+    (phat + z*z/(2*total) - z * Math.sqrt((phat*(1-phat)+z*z/(4*total))/total))/(1+z*z/total)
   end
 
   ###################################################
