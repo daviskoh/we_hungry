92be4cbcd85c58dc0ce1de33b0b405a5a1e3d756
daviskoh <koh.davis.0@gmail.com>
2014-01-15 02:07:07 -0500

    heroku actually fast

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ def ingredient_in_db?(ingredient)
   !Ingredient.where('LOWER(name) = LOWER(?)', ingredient).empty?
 end
 
-response = Yummly.search("main", maxResult: 101, start: 2200)
+response = Yummly.search("main", maxResult: 1000, start: 2300)
 
 response.each do |recipe|
   # food = unless food_in_db?(recipe)
