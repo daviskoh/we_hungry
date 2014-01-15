d19f1d2924e9486f1f9cd97b53c1b19618895631
daviskoh <koh.davis.0@gmail.com>
2014-01-15 00:51:06 -0500

    user reco starts w/ random ingredients

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,16 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  def top_ingredients(amount = 2)
-    # default to LIMIT 2
+  def top_ingredients(amount = 2) # default to LIMIT 2
     ci_lower_bound_query = "((pos_votes + 1.9208) / tot_votes - 1.96 * SQRT((pos_votes * (tot_votes - pos_votes)) / tot_votes + 0.9604) / tot_votes) / (1 + 3.8416 / tot_votes) DESC LIMIT #{amount}"
 
     relationships = IngredientsUsers.where('user_id = ? AND tot_votes > 0', self.id).order(ci_lower_bound_query)
 
-    # must change below if want amount to be dynamic
-    Ingredient.where("id = ? OR id = ?", relationships[0].ingredient_id, relationships[1].ingredient_id)
+    if relationships.empty?
+      Ingredient.order('random() LIMIT 2')
+    else
+      # must change below if want amount to be dynamic
+      Ingredient.where("id = ? OR id = ?", relationships[0].ingredient_id, relationships[1].ingredient_id)
+    end
   end
 end
