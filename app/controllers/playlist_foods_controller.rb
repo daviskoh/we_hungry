2efefed35a18cd0f84c43d48d161190c0e40bea9
daviskoh <koh.davis.0@gmail.com>
2014-02-03 01:47:53 -0500

    functioning upvote

--- app/controllers/playlist_foods_controller.rb
+++ app/controllers/playlist_foods_controller.rb
@@ -1,13 +1,14 @@
 class PlaylistFoodsController < ApplicationController
   include RecommendationsHelper
+  before_action :get_food_info
 
   def show
-    get_food_info
   end
 
-  def like
-    get_food_info
+  #TODO change below to api
 
+  def like
+    binding.pry
     @food.ingredients.each do |ing|
       relation = IngredientsUsers.where(user_id: current_user.id, ingredient_id: ing.id)[0]
       relation.pos_votes += 1
@@ -21,8 +22,7 @@ class PlaylistFoodsController < ApplicationController
   end
 
   def dislike
-    get_food_info
-
+    binding.pry
     @food.ingredients.each do |ing|
       relation = IngredientsUsers.where(user_id: current_user.id, ingredient_id: ing.id)[0]
       relation.tot_votes += 1
@@ -33,6 +33,8 @@ class PlaylistFoodsController < ApplicationController
 
     redirect_to user_path(current_user)
   end
+
+  private
 
   def get_food_info
     @food = PlaylistFood.find(params[:id])
